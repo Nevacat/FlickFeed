2b23cf57a143414a7d560d769a80e17b361ebda5
Nevacat <nevacattery217@naver.com>
2023-04-17 13:33:21 +0900

    Docs: theme에 color 부분 오타 수정
    lightPuple -> lightPurple

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,7 +16,7 @@ const color = {
   navy: "#45537c",
   gray: "#B3B6C4",
   purple: "#a77afe",
-  lightPuple:"#dcc8fa",
+  lightPurple:"#dcc8fa",
   skyblue:"#58bafe",
   white: "#ffffff",
   black: "#181818"
