c6b68ac579473834982a55f7f6f0dbee58d64599
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-04-19 10:45:34 +0900

    feat: 댓글 아이콘 생성

--- src/components/Feed/CommentIcon/style.ts
+++ src/components/Feed/CommentIcon/style.ts
@@ -1,1 +1,8 @@
 import styled from 'styled-components';
+import { FaRegComment } from 'react-icons/fa';
+
+export const CommentIcon = styled(FaRegComment)`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+`;
