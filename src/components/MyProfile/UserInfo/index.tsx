ea493674f99e5846d7514c8713e298bfb3fe1fbb
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-04-20 17:26:07 +0900

    feat: MyProfile의 UserInfo 작업

---
+++ src/components/MyProfile/UserInfo/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import * as S from './style';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+function index() {
+  const getUser = async () => {
+    const response = await axios({
+      method: 'POST',
+      url: '/auth/me',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywidXNlcm5hbWUiOiJhbGljZSIsImVtYWlsIjoidGVzdEBuYXZlci5jb20iLCJpYXQiOjE2ODE1MzY5MzAsImV4cCI6MTY4MTU0MDUzMH0.s0km3UHRLyTh8pR2sKxludFTbmRH_nNvvp0x_kDxpVI',
+      },
+    });
+    return response.data;
+  };
+
+  const { data: user } = useQuery(['user'], getUser, {
+    onSuccess: (user) => {
+      console.log(user);
+    },
+  });
+
+  return (
+    <S.UserInfo>
+      <S.Image>
+        <img
+          src={
+            user?.userImage ? user?.userImage : 'images/profile_background.jpg'
+          }
+          alt={user?.username}
+        />
+      </S.Image>
+      <S.Name>{user?.username}</S.Name>
+      <S.StatusMessage>{user?.userInfo}</S.StatusMessage>
+
+      <S.EditButton type="button">프로필 편집</S.EditButton>
+    </S.UserInfo>
+  );
+}
+
+export default index;
--- src/pages/MyProfile/index.tsx
+++ src/pages/MyProfile/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Background,
+  UserInfo,
+  CardList,
+  Header,
+  Modal,
+} from '../../components/MyProfile/';
+import axios from 'axios';
+import { useQuery } from 'react-query';
 
 function MyProfile() {
   return (
-    <div>MyProfile</div>
-  )
+    <main style={{ position: 'relative' }}>
+      <Header />
+      <Background />
+      <UserInfo />
+    </main>
+  );
 }
 
-export default MyProfile
+export default MyProfile;
