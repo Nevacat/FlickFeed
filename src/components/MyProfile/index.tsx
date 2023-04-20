d43491ff8730f6e473eb957bb8509115e4ab38bf
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-04-20 17:32:19 +0900

    feat: MyProfile의 컴포넌트들 내보내기

---
+++ src/components/MyProfile/index.tsx
@@ -0,0 +1,7 @@
+import Header from './Header';
+import Background from './Background';
+import UserInfo from './UserInfo';
+import Modal from './Modal';
+import CardList from './CardList';
+
+export { Header, Background, UserInfo, Modal, CardList };
