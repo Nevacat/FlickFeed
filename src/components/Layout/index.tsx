a96adbd4198b5b73835d5e90e6f6bd9d5363fd8f
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-04-20 17:14:59 +0900

    feat: myProfile 라우팅 작업

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from '../Common/Nav';
 import * as S from './style';
+import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
     <S.Layout>
+      <Outlet />
       <Nav />
     </S.Layout>
   );
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
+import MyProfile from '../pages/MyProfile';
 
 function Routers() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route path="/myProfile" element={<MyProfile />} />
+      </Route>
     </Routes>
   );
 }
