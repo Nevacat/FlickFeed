f74605daf8885235bc69387caa50102e65d0fff0
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-04-21 19:53:42 +0900

    remove: Body 컴포넌트 삭제
    - Body 컴포넌트가  UsersCarousel, PostList wrapper 기능했으나, Feed 페이지 안에 UsersCarousel, PostList 바로 넣는 형식으로 변경

--- src/pages/Feed/index.tsx
+++ src/pages/Feed/index.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import UsersCarousel from '../../components/Feed/UsersCarousel';
 import PostList from '../../components/Feed/PostList';
 import * as S from './style';
-import Body from '../../components/Feed/Body';
 
 function Feed() {
   return (
     <S.Feed>
-      <Body>
-        <UsersCarousel />
-        <PostList />{' '}
-      </Body>
+      <UsersCarousel />
+      <PostList />
     </S.Feed>
   );
 }
