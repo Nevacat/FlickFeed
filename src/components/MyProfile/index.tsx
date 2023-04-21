643aafdeb51bf591b94f709776c3c2675631e038
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-04-21 20:36:31 +0900

    fix: 폴더 이름 변경
    UserInfoModal

--- src/components/MyProfile/index.tsx
+++ src/components/MyProfile/index.tsx
@@ -1,7 +1,7 @@
 import Header from '../Common/Header';
 import Background from './Background';
 import UserInfo from './UserInfo';
-import Modal from './Modal';
+import UserInfoModal from './UserInfoModal';
 import CardList from './CardList';
 
-export { Header, Background, UserInfo, Modal, CardList };
+export { Header, Background, UserInfo, UserInfoModal, CardList };
