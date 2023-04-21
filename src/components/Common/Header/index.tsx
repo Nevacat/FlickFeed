dfa281ed758636aca3c9fa1bc113b427a8a7f044
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-04-21 19:58:58 +0900

    feat: 로고 및 로그아웃 버튼 기능 구현

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { VscChevronLeft } from 'react-icons/vsc';
 import { AiFillGithub } from 'react-icons/ai';
 import * as S from './style';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { BiLogOut } from 'react-icons/bi';
+import axios from 'axios';
 
 function index() {
   const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location);
+
+  const logoutHandler = async () => {
+    const response = await axios.post('/logout');
+    // navigate('/login');
+    return response.data;
+  };
 
   return (
     <S.Header>
-      <S.Button onClick={() => navigate(-1)}>
+      <S.BackButton location={location} onClick={() => navigate(-1)}>
         <VscChevronLeft />
-      </S.Button>
+      </S.BackButton>
+      <S.LogoutButton location={location} onClick={logoutHandler}>
+        <BiLogOut />
+      </S.LogoutButton>
+
+      <S.Logo location={location}>
+        <img src="images/FlickFeed.png" alt="FilckFeed" />
+      </S.Logo>
 
       <S.StyledLink to="https://github.com/Nevacat/FlickFeed">
         <AiFillGithub />
