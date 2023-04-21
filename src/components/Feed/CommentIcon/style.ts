0120fb000c9f455b4ed28846c6ca93937a9c515f
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-04-21 17:18:32 +0900

    remove: 파일 삭제

--- src/components/Feed/CommentIcon/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { FaRegComment } from 'react-icons/fa';
-
-export const CommentIcon = styled(FaRegComment)`
-  width: 20px;
-  height: 20px;
-  cursor: pointer;
-`;
