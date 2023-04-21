e96c5e32b60df90bb7529866d0c22175da81b8c8
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-04-21 17:05:46 +0900

    design: 댓글 input UI 변경
    - focus할때 outline 없앰
    - 높이 및 너비 확대

--- src/components/Feed/Comments/style.ts
+++ src/components/Feed/Comments/style.ts
@@ -1,5 +1,32 @@
 import styled from 'styled-components';
 
 export const Comments = styled.div`
-  height: 40px;
+  overflow: auto;
+  border: 1px solid ${({ theme }) => theme.color.gray};
+`;
+
+export const Form = styled.form`
+  position: relative;
+  height: 100px;
+`;
+export const CommentWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  width: 100%;
+`;
+export const Input = styled.input`
+  width: 85%;
+  height: 30px;
+  border: none;
+  padding: 10px;
+  &:focus {
+    outline: none;
+  }
+`;
+export const Button = styled.button`
+  border: none;
+  color: #3e729b;
+  background: #fff;
+  width: 15%;
 `;
