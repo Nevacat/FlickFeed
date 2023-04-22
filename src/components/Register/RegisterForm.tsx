fe59b16464b373358f0ff4cf06981835ae0e31da
hlee686 <heso.lee@gmail.com>
2023-04-22 21:17:12 +0900

    feat: 비밀번호확인, 유저이미지 추가 구현

--- src/components/Register/RegisterForm.tsx
+++ src/components/Register/RegisterForm.tsx
@@ -3,7 +3,7 @@ import { UseMutateFunction } from 'react-query'
 import { AuthResponse, LoginRequest , RegisterRequest} from '../../interface/auth'
 import { AxiosError } from 'axios'
 import { getCookie} from '../../utils/cookies'
-import { ContainerStyle, EmailStyle, PwStyle, UsernameStyle, SignupStyle, Signup, ImgStyle, Warning} from './style'
+import { ContainerStyle, EmailStyle, PwStyle, UsernameStyle, SignupStyle, Signup, ImgStyle, Warning, PasswordConfirm, FileSubmit} from './style'
 import { useNavigate } from 'react-router-dom'
 
 export interface RegisterFormProps {
@@ -12,21 +12,31 @@ export interface RegisterFormProps {
 
 function RegisterForm({ mutate }: RegisterFormProps) {
   let navigate = useNavigate()
-  const [userInput, setUserInput] = useState<RegisterRequest>({ email: '', password: '' , username:''})
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+  const [userInput, setUserInput] = useState<RegisterRequest>({ email: '', password: '' , username:'', passwordConfirm: '',selectedFile: null })
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     setUserInput({ ...userInput, [name]: value })
   }
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    mutate(userInput)
-    console.log(userInput)
-    alert("All Signed Up!")
-    setUserInput({email: '', password: '', username: ''})
-    navigate("/login")
-
+    if(userInput.password == userInput.passwordConfirm){
+      e.preventDefault()
+      mutate(userInput)
+      console.log(userInput)
+      alert("All Signed Up!")
+      setUserInput({email: '', password: '', username: '', passwordConfirm: '', selectedFile: null})
+      navigate("/login")
+    }else{
+      alert("비밀번호를 확인해주세요!")
+    }
   }
-
+  const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    setUserInput(prevState => ({
+      ...prevState,
+      selectedFile: file || null
+    }))
+  }
 
   return (
     <ContainerStyle onSubmit={onSubmit} className="container" >
@@ -36,10 +46,16 @@ function RegisterForm({ mutate }: RegisterFormProps) {
       <EmailStyle name="email" type="email" value={userInput.email} onChange={onChange} className="email" placeholder="이메일"/>
       </div>
       <div>
-      <PwStyle name="password" type="password" value={userInput.password} onChange={onChange} className="password" placeholder="비밀번호"/>
+      <PwStyle name="password" type="password" value={userInput.password} onChange={onChange} className="password" placeholder="비밀번호" pattern=".{8,}" required/>
+      </div>
+      <div>
+        <PasswordConfirm name="passwordConfirm" type="password" value={userInput.passwordConfirm} onChange={onChange} className="passwordConfirm" placeholder="비밀번호 확인" pattern=".{8,}" required/>
       </div>
       <div>
         <UsernameStyle type="text" name="username" placeholder="유저네임" value={userInput.username} onChange={onChange} className="username" />
+      </div>
+      <div>
+        <FileSubmit type="file" accept="image/*" onChange={handleFileSelect}/>
       </div>
       <SignupStyle type="submit" className="signUp">회원가입</SignupStyle>
       <Warning>Get started for free. No spam of affiliate links</Warning>
