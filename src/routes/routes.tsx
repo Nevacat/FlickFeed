04598ee8e4dd812568cd5f9592ca988aa32b4d93
hlee686 <heso.lee@gmail.com>
2023-04-23 11:48:02 +0900

    feat: 변경 커밋

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Outlet} from "react-router-dom"
 import Layout from '../components/Layout'
+import LoginForm from "../components/Login/LoginForm"
+import LoginPage from "../pages/Login/LoginPage"
+import RegisterPage from "../pages/Register/RegisterPage"
 
 function Routers() {
   return (
     <Routes>
       <Route path="/" element={<Layout/>}>
+        <Route index path="login" element={<LoginPage/>} />
+        <Route path="/posts" element={<Layout />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
-
-  )
+  );
 }
 
 export default Routers
