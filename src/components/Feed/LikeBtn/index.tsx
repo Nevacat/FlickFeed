6ac8f5f72b10296b92884db278680bb43099b97c
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-04-25 15:36:11 +0900

    feat: 새로고침해도 좋아요 유지 기능 생성
    - lazy initial, localStorage 사용

--- src/components/Feed/LikeBtn/index.tsx
+++ src/components/Feed/LikeBtn/index.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './style';
-import { useState } from 'react';
 import { AiOutlineHeart, AiTwotoneHeart } from 'react-icons/ai';
 import { IconContext } from 'react-icons';
 import { useMutation } from 'react-query';
-import axios from 'axios';
 import { postLikes } from '../../../api/data';
 
 function LikeBtn({ postId }) {
-  const [isLiked, setIsLiked] = useState(false);
+  const [isLiked, setIsLiked] = useState(() => {
+    const likeStatus = localStorage.getItem(`post-${postId}`);
+    return likeStatus ? JSON.parse(likeStatus) : false;
+  });
   const { mutate, isLikeLoading, error } = useMutation((post) =>
     postLikes(post)
   );
+
+  useEffect(() => {
+    localStorage.setItem(`post-${postId}`, JSON.stringify(isLiked));
+  }, [postId, isLiked]);
 
   const toggleLike = () => {
     mutate(postId);
