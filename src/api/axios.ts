fa50c632efb33b4a9e8d8e45d64bb49cbaed7fc6
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-04-25 21:33:56 +0900

    fix: baseURL 주소 변경
    .env 파일의 경로 참조

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,28 +1,28 @@
-import { getCookie } from '../utils/cookies'
-import axios, { AxiosError, AxiosRequestConfig } from 'axios'
+import { getCookie } from '../utils/cookies';
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
 const getAxiosInstance = () => {
-  const config: AxiosRequestConfig = { 
-    baseURL: "http://localhost:5173",
+  const config: AxiosRequestConfig = {
+    baseURL: import.meta.env.VITE_SERVER_URL,
     headers: {
       'Content-type': 'application/json',
     },
     withCredentials: true,
-  }
-  const instance = axios.create(config) 
-  instance.defaults.timeout = 3000
+  };
+  const instance = axios.create(config);
+  instance.defaults.timeout = 3000;
   instance.interceptors.request.use(
     (request) => {
-      const token = getCookie('accessToken')
-      if (token) request.headers['Authorization'] = `Bearer ${token}`
-      return request
+      const token = getCookie('accessToken');
+      if (token) request.headers['Authorization'] = `Bearer ${token}`;
+      return request;
     },
-    (error: AxiosError) => { 
-      console.log(error)
-      return Promise.reject(error)
+    (error: AxiosError) => {
+      console.log(error);
+      return Promise.reject(error);
     }
-  )
-  return instance
-}
+  );
+  return instance;
+};
 
-export const axiosInstance = getAxiosInstance()
+export const axiosInstance = getAxiosInstance();
