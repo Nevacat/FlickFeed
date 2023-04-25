8e18acf10c432a7f57509176ff3b013567e39f05
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-04-25 15:25:19 +0900

    feat: user 관련 api 생성

--- src/api/data.ts
+++ src/api/data.ts
@@ -1,1 +1,19 @@
 // API 를 불러오는 곳
+
+import { UserInput } from '../interface/user';
+import { axiosInstance } from './axios';
+
+export const getUser = async () => {
+  const response = await axiosInstance.post('/auth/me');
+  return response.data;
+};
+
+export const getPostsByUser = async () => {
+  const response = await axiosInstance.get('/posts/me');
+  return response.data;
+};
+
+export const editUser = async (input: UserInput) => {
+  const response = await axiosInstance.put('/users', input);
+  return response.data;
+};
