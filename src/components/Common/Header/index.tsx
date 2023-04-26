1b87a2322481fa5205eea660c295c67942a49a71
PYO SEUNGYEON <pyo.seungyeonn@gmail.com>
2023-04-26 14:43:49 +0900

    feat: LogoutConfirmModal 컴포넌트 구현
    - 로그아웃 더블체크 기능

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { VscChevronLeft } from 'react-icons/vsc';
 import { AiFillGithub } from 'react-icons/ai';
 import * as S from './style';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { BiLogOut } from 'react-icons/bi';
-import { axiosInstance } from '../../../api/axios';
-import { removeCookie } from '../../../utils/cookies';
+import LogoutConfirmModal from '../LogoutConfirmModal';
 
 function index() {
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
   console.log(location);
 
   const logoutHandler = async () => {
-    const response = await axiosInstance.post('/auth/logout');
-    removeCookie('accessToken');
-    navigate('/');
-    return response.data;
+    setIsLogoutModalOpen(true);
   };
 
   return (
@@ -27,6 +24,10 @@ function index() {
       <S.LogoutButton location={location} onClick={logoutHandler}>
         <BiLogOut />
       </S.LogoutButton>
+
+      {isLogoutModalOpen && (
+        <LogoutConfirmModal setIsLogoutModalOpen={setIsLogoutModalOpen} />
+      )}
 
       <S.Logo location={location}>
         <S.Img src="images/FlickFeed.png" alt="FilckFeed" />
