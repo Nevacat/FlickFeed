e2017d784dba1d1b9908b77e4260deab4d391f8d
hlee686 <heso.lee@gmail.com>
2023-04-26 18:09:03 +0900

    feat: 수정

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,9 @@
+import Header from '../Common/Header'
+import Nav from '../Common/Nav'
 import React from 'react'
 import * as S from './style'
 import { Outlet } from 'react-router-dom'
+
 function Layout() {
   return (
     <S.Layout>
